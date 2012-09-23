e59f6ba58eaf4789c5b9b998d21bd7177e4167a7
John Jacobsen <john@mail.npxdesigns.com>
2012-09-22 22:14:13 -0500

    Move to new CC / lein2 install

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject probs4clojure "1.0.0-SNAPSHOT"
-  :main probs4clojure.core
+(defproject probs4clojure "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [expectations "1.3.3"]
-                 [org.clojure/tools.trace "0.7.3"]])
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
