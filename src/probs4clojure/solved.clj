79fb766e120e9bf255991ee3bd28d7bf7cd05440
John Jacobsen <john@mail.npxdesigns.com>
2013-05-20 22:21:29 -0500

    Problem 73

--- src/probs4clojure/solved.clj
+++ src/probs4clojure/solved.clj
@@ -2,6 +2,88 @@
   (:use expectations))
 
 
+;; Problem 73: Analyze a Tic-Tac-Toe Board
+ 
+;; Difficulty:	Hard
+;; Topics:	game
+
+
+;; A tic-tac-toe board is represented by a two dimensional vector. X is represented by :x, O is represented by :o, and empty is represented by :e. A player wins by placing three Xs or three Os in a horizontal, vertical, or diagonal row. Write a function which analyzes a tic-tac-toe board and returns :x if X has won, :o if O has won, and nil if neither player has won.
+	
+
+;; (= nil (__ [[:e :e :e]
+;;             [:e :e :e]
+;;             [:e :e :e]]))
+	
+
+;; (= :x (__ [[:x :e :o]
+;;            [:x :e :e]
+;;            [:x :e :o]]))
+	
+
+;; (= :o (__ [[:e :x :e]
+;;            [:o :o :o]
+;;            [:x :e :x]]))
+	
+
+;; (= nil (__ [[:x :e :o]
+;;             [:x :x :e]
+;;             [:o :x :o]]))
+	
+
+;; (= :x (__ [[:x :e :e]
+;;            [:o :x :e]
+;;            [:o :e :x]]))
+	
+
+;; (= :o (__ [[:x :e :o]
+;;            [:x :o :e]
+;;            [:o :e :x]]))
+	
+
+;; (= nil (__ [[:x :o :x]
+;;             [:x :o :x]
+;;             [:o :x :o]]))
+
+
+((fn winner [board]
+   (letfn [(fcombos [board]
+             (concat
+              ;; by rows
+              (for [j (range 3)]
+                (for [i (range 3)]
+                  ((board i) j)))
+              
+              ;; by columns
+              (for [i (range 3)]
+                (for [j (range 3)]
+                  ((board i) j)))
+              
+              ;; crosswise
+              (list (for [i (range (count board))]
+                      (nth (board i) i)))
+              
+              (list (for [i (range (count board))]
+                      (nth (board i) (- 2 i))))))]
+     (cond
+      (reduce #(or %1 %2) (map #(apply = (list* :x %)) (fcombos board))) :x
+      (reduce #(or %1 %2) (map #(apply = (list* :o %)) (fcombos board))) :o)))
+
+  [[:x :o :x]
+   [:o :o :o]
+   [:o :x :x]])
+
+(winner [[:x :o :x]
+         [:x :o :x]
+         [:o :x :x]])
+
+(winner [[:o :e :e]
+         [:o :x :e]
+         [:o :e :x]])
+
+
+
+
 ;; Problem 53
 ;; Difficulty:	Hard
 ;; Topics:	seqs
