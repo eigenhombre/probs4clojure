ef9ebcbc08cd23b6998266d0e634e56685d68c2b
John Jacobsen <jjacobsen@opinionlab.com>
2014-07-18 14:51:35 -0500

    Add extra test for 141.

--- test/probs4clojure/core_test.clj
+++ test/probs4clojure/core_test.clj
@@ -1214,7 +1214,10 @@
                                          {:suit :club :rank 10}]))
   (= {:suit :heart :rank 8}
      ((__ :heart) [{:suit :heart :rank 6} {:suit :heart :rank 8}
-                   {:suit :diamond :rank 10} {:suit :heart :rank 4}])))
+                   {:suit :diamond :rank 10} {:suit :heart :rank 4}]))
+  (= {:suit :diamond :rank 2}
+     ((__ :club) [{:suit :diamond :rank 2}
+                  {:suit :spade :rank 10}])))
 
 
 
