61f077a75b64b13ab34763fd8140f7a4feb9b51d
John Jacobsen <jjacobsen@opinionlab.com>
2014-09-08 14:07:19 -0500

    Added 144

--- test/probs4clojure/core_test.clj
+++ test/probs4clojure/core_test.clj
@@ -1779,8 +1779,21 @@
 
 ;; ### Problem 144: <a href="https://www.4clojure.com/problem/144">Oscilrate<a/>
 ;;
-;;
-(solves  )
+;; The `cycle` function (1), applied to the list of given functions, makes
+;; this straightforward.
+(solves
+  (fn f [x & s]
+    (letfn [(g [x s]
+              (lazy-seq
+               (cons x
+                     (g ((first s) x)
+                        (next s)))))]
+      (g x (cycle s))))                    ;; #1
+
+  (= (take 3 (__ 3.14 int double)) [3.14 3 3.0])
+  (= (take 5 (__ 3 #(- % 3) #(+ 5 %))) [3 0 5 2 7])
+  (= (take 12 (__ 0 inc dec inc dec inc)) [0 1 0 1 0 1 2 1 2 1 2 3]))
+
 
 ;; ### Problem 150: <a href="http://www.4clojure.com/problem/150">Palindromic Numbers</a>
 ;;
