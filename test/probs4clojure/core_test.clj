75718472b669a57da2403bad5889806513cafeae
John Jacobsen <john@mail.npxdesigns.com>
2014-10-12 14:38:57 -0500

    Problem 164 (whew\!)

--- test/probs4clojure/core_test.clj
+++ test/probs4clojure/core_test.clj
@@ -1954,6 +1954,101 @@
      9102019))
 
 
+;; ### Problem 164: <a href="http://www.4clojure.com/problem/164">Language of a DFA</a>
+;;
+;; My first attempt at this was (I realized, too late) a recursive
+;; depth-first search.  This doesn't work (as the warning in the
+;; problem description suggests) because some of the examples never
+;; "bottom out" -- they keep generating strings infinitely.  This
+;; obviously suggests that something more lazy is needed.  It took a
+;; bit of fiddling to figure out the basic ingredients:
+;;
+;; 1. Create a `lazy-seq`-based function which represents each stage
+;;    of a breadth-first search, collecting all the current states and
+;;    emitting (`cons`ing) a record with all accumulated letters
+;;    whenever a terminal state is reached.
+;; 2. For the letters, accumulate symbols rather than strings; convert
+;;    to strings at the end (strings and laziness can be a poor match,
+;;    as any lazy object gets converted to something like
+;;    "clojure.lang.LazySeq@c5d38b66" rather than what you want.
+;;
+;; The inputs are overdetermined; `:states` and `:alphabet` are
+;; redundant and not used.
+(solves
+  (fn [m]
+    (letfn [(stateseq [chains]
+              (lazy-seq
+               (let [transitions (-> m :transitions)
+                     next-chains (for [{:keys [state letters]} chains
+                                       [letter next-state] (transitions state)]
+                                   {:terminal (-> m :accepts next-state)
+                                    :state next-state
+                                    :letters (conj letters letter)})
+                     terminals (filter :terminal next-chains)]
+                 (when (seq next-chains)
+                   (concat terminals (stateseq next-chains))))))]
+      (map (comp (partial apply str) :letters)
+           (stateseq [{:state (:start m), :letters []}]))))
+
+  (= #{"a" "ab" "abc"}
+     (set (__ '{:states #{q0 q1 q2 q3}
+                :alphabet #{a b c}
+                :start q0
+                :accepts #{q1 q2 q3}
+                :transitions {q0 {a q1}
+                              q1 {b q2}
+                              q2 {c q3}}})))
+  (= #{"hi" "hey" "hello"}
+     (set (__ '{:states #{q0 q1 q2 q3 q4 q5 q6 q7}
+                :alphabet #{e h i l o y}
+                :start q0
+                :accepts #{q2 q4 q7}
+                :transitions {q0 {h q1}
+                              q1 {i q2, e q3}
+                              q3 {l q5, y q4}
+                              q5 {l q6}
+                              q6 {o q7}}})))
+  (= (set (let [ss "vwxyz"] (for [i ss, j ss, k ss, l ss] (str i j k l))))
+     (set (__ '{:states #{q0 q1 q2 q3 q4}
+                :alphabet #{v w x y z}
+                :start q0
+                :accepts #{q4}
+                :transitions {q0 {v q1, w q1, x q1, y q1, z q1}
+                              q1 {v q2, w q2, x q2, y q2, z q2}
+                              q2 {v q3, w q3, x q3, y q3, z q3}
+                              q3 {v q4, w q4, x q4, y q4, z q4}}})))
+  (let [res (take 2000 (__ '{:states #{q0 q1}
+                             :alphabet #{0 1}
+                             :start q0
+                             :accepts #{q0}
+                             :transitions {q0 {0 q0, 1 q1}
+                                           q1 {0 q1, 1 q0}}}))]
+    (and (every? (partial re-matches #"0*(?:10*10*)*") res)
+         (= res (distinct res))))
+  (let [res (take 2000 (__ '{:states #{q0 q1}
+                             :alphabet #{n m}
+                             :start q0
+                             :accepts #{q1}
+                             :transitions {q0 {n q0, m q1}}}))]
+    (and (every? (partial re-matches #"n*m") res)
+         (= res (distinct res))))
+  (let [res (take 2000 (__ '{:states #{q0 q1 q2 q3 q4 q5 q6 q7 q8 q9}
+                             :alphabet #{i l o m p t}
+                             :start q0
+                             :accepts #{q5 q8}
+                             :transitions {q0 {l q1}
+                                           q1 {i q2, o q6}
+                                           q2 {m q3}
+                                           q3 {i q4}
+                                           q4 {t q5}
+                                           q6 {o q7}
+                                           q7 {p q8}
+                                           q8 {l q9}
+                                           q9 {o q6}}}))]
+    (and (every? (partial re-matches #"limit|(?:loop)+") res)
+         (= res (distinct res)))))
+
+
 ;; ### Problem 168: <a href="http://www.4clojure.com/problem/168">Infinite Matrix</a>
 ;;
 ;; This problem is all about `lazy-seq` and would probably be hard to
@@ -2126,4 +2221,3 @@
   (= :full-house (__ ["HA" "DA" "CA" "HJ" "DJ"]))
   (= :four-of-a-kind (__ ["HA" "DA" "CA" "SA" "DJ"]))
   (= :straight-flush (__ ["HA" "HK" "HQ" "HJ" "HT"])))
-
