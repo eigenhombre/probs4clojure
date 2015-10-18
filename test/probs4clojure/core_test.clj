51d6d25d51181803a666fc35b591105b253a1bb7
John Jacobsen <john@mail.npxdesigns.com>
2015-10-18 08:09:33 -0500

    Merge branch 'master' of github.com:eigenhombre/probs4clojure

--- test/probs4clojure/core_test.clj
+++ test/probs4clojure/core_test.clj
@@ -1324,6 +1324,24 @@
   (= 5 (__ 9 12))) ; 9 11 22 24 12
 
 
+;; ### Problem 108:
+(solves
+ (fn c [& seqs]
+   (let [firsts (map first seqs)
+         biggest (apply max firsts)
+         trim #(if (> biggest (first %)) (rest %) %)]
+     (if (apply = firsts)
+       (first firsts)
+       (recur (map trim seqs)))))
+
+ (= 3 (__ [3 4 5]))
+ (= 4 (__ [1 2 3 4 5 6 7] [0.5 3/2 4 19]))
+ (= 7 (__ (range) (range 0 100 7/6) [2 3 5 7 11 13]))
+ (= 64 (__ (map #(* % % %) (range))
+           (filter #(zero? (bit-and % (dec %))) (range))
+           (iterate inc 20))))
+
+
 ;; ### Problem 111: <a href="http://www.4clojure.com/problem/111">Crossword Puzzle</a>
 ;;
 ;; The approach taken here is a relatively straightforward functional
