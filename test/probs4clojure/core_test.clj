e0e079b2c6001c8199441a540f29c3e00bcc8f0a
John Jacobsen <john@mail.npxdesigns.com>
2017-10-16 07:56:42 -0500

    "Modernize"...
    convert to clojure.test; record problem number in the macro. (WIP)

--- test/probs4clojure/core_test.clj
+++ test/probs4clojure/core_test.clj
@@ -1,5 +1,5 @@
 (ns probs4clojure.core-test
-  (:require [probs4clojure.test-util :refer [solves]]))
+  (:require [probs4clojure.test-util :refer [solves problem]]))
 
 ;; ### Solutions to 4clojure.com problems
 ;;
@@ -15,84 +15,83 @@
 ;; </script>
 
 
-;; ### Problem 1:
-(solves true (= __ true))
+(problem 1
+  true (= __ true))
 
 
-;; ### Problem 2:
-(solves 4
- (= (- 10 (* 2 3)) __))
+(problem 2
+  4
+  (= (- 10 (* 2 3)) __))
 
 
-;; ### Problem 3:
-(solves "HELLO WORLD"
- (= __ (.toUpperCase "hello world")))
+(problem 3
+  "HELLO WORLD"
+  (= __ (.toUpperCase "hello world")))
 
 
-;; ### Problem 4:
-;; This is the one problem not amenable to `solves` off-the-shelf, so
+;; This is the one problem not amenable to `problem` off-the-shelf, so
 ;; solution is added inline.
-(solves
+(problem 4
  (= (list :a :b :c) '(:a :b :c)))
 
 
-;; ### Problem 5:
-(solves '(1 2 3 4)
- (= __ (conj '(2 3 4) 1))
- (= __ (conj '(3 4) 2 1)))
+(problem 5
+  '(1 2 3 4)
+  (= __ (conj '(2 3 4) 1))
+  (= __ (conj '(3 4) 2 1)))
 
 
-;; ### Problem 6:
-(solves [:a :b :c]
- (= (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c)))
+(problem 6
+  [:a :b :c]
+  (= (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c)))
 
 
-;; ### Problem 7:
-(solves [1 2 3 4]
- (= __ (conj [1 2 3] 4))
- (= __ (conj [1 2] 3 4)))
+(problem 7
+  [1 2 3 4]
+  (= __ (conj [1 2 3] 4))
+  (= __ (conj [1 2] 3 4)))
 
 
-;; ### Problem 8:
-(solves #{:a :b :c :d}
- (= __ (set '(:a :a :b :c :c :c :c :d :d)))
- (= __ (clojure.set/union #{:a :b :c} #{:b :c :d})))
+(problem 8
+  #{:a :b :c :d}
+  (= __ (set '(:a :a :b :c :c :c :c :d :d)))
+  (= __ (clojure.set/union #{:a :b :c} #{:b :c :d})))
 
 
-;; ### Problem 9:
-(solves 2
- (= #{1 2 3 4} (conj #{1 4 3} __)))
+(problem 9
+  2
+  (= #{1 2 3 4} (conj #{1 4 3} __)))
 
 
-;; ### Problem 10:
-(solves 20
- (= __ ((hash-map :a 10, :b 20, :c 30) :b))
- (= __ (:b {:a 10, :b 20, :c 30})))
+(problem 10
+  20
+  (= __ ((hash-map :a 10, :b 20, :c 30) :b))
+  (= __ (:b {:a 10, :b 20, :c 30})))
 
 
-;; ### Problem 11:
-(solves [:b 2]
- (= {:a 1, :b 2, :c 3} (conj {:a 1} __ [:c 3])))
+(problem 11
+  [:b 2]
+  (= {:a 1, :b 2, :c 3} (conj {:a 1} __ [:c 3])))
 
 
-;; ### Problem 12:
-(solves 3
- (= __ (first '(3 2 1)))
- (= __ (second [2 3 4]))
- (= __ (last (list 1 2 3))))
+(problem 12
+  3
+  (= __ (first '(3 2 1)))
+  (= __ (second [2 3 4]))
+  (= __ (last (list 1 2 3))))
 
 
-;; ### Problem 13:
-(solves [20 30 40]
- (= __ (rest [10 20 30 40])))
+(problem 13
+  [20 30 40]
+  (= __ (rest [10 20 30 40])))
 
 
-;; ### Problem 14:
-(solves 8
- (= __ ((fn add-five [x] (+ x 5)) 3))
- (= __ ((fn [x] (+ x 5)) 3))
- (= __ (#(+ % 5) 3))
- (= __ ((partial + 5) 3)))
+(problem 14
+  8
+  (= __ ((fn add-five [x] (+ x 5)) 3))
+  (= __ ((fn [x] (+ x 5)) 3))
+  (= __ (#(+ % 5) 3))
+  (= __ ((partial + 5) 3)))
 
 
 ;; ### Problem 15:
--- test/probs4clojure/test_util.clj
+++ test/probs4clojure/test_util.clj
@@ -1,5 +1,6 @@
 (ns probs4clojure.test-util
   (:require [speclj.core :refer :all]
+            [clojure.test :refer [deftest testing is]]
             [clojure.walk :refer [postwalk-replace]]))
 
 
@@ -14,9 +15,30 @@
   [expr & tests]
   (let [replacef# (fn [t] (postwalk-replace {'__ expr} t))
         newtests# (map replacef# tests)]
-    `(describe ~(str expr)
+    #_`(deftest ~(symbol (str "test-" (rand-int 100000)))
+       (println '~expr)
        ~@(for [t# newtests#
-               :let [should# `(should ~t#)
+               :let [should# `(is ~t#)
                      shouldstr# (str t#)]]
-           `(it ~(str "should " shouldstr#)
+           `(testing ~(str shouldstr#)
+              ~should#)))))
+
+
+(defmacro problem
+  "
+  The `problem` macro presents and tests a solution to a
+  [4clojure.com](http://4clojure.com) problem by taking the first
+  expression, substituting it for __ in all subsequent expressions,
+  and evaluating each resulting expression for truthiness (i.e.,
+  evaluating the resulting Speclj assertions).
+  "
+  [problem-number expr & tests]
+  (let [replacef# (fn [t] (postwalk-replace {'__ expr} t))
+        newtests# (map replacef# tests)]
+    `(deftest ~(symbol (str "problem-" problem-number))
+       (println (str "problem " ~problem-number))
+       ~@(for [t# newtests#
+               :let [should# `(is ~t#)
+                     shouldstr# (str t#)]]
+           `(testing ~(str shouldstr#)
               ~should#)))))
