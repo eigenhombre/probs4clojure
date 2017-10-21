99086b7b4ff39728651183c8b06e9ad02f62fe56
John Jacobsen <john@mail.npxdesigns.com>
2017-10-20 19:04:29 -0500

    Whitespace.

--- test/probs4clojure/core_test.clj
+++ test/probs4clojure/core_test.clj
@@ -1568,7 +1568,6 @@
       (if (apply = firsts)
         (first firsts)
         (recur (map trim seqs)))))
-
   (= 3 (__ [3 4 5]))
   (= 4 (__ [1 2 3 4 5 6 7] [0.5 3/2 4 19]))
   (= 7 (__ (range) (range 0 100 7/6) [2 3 5 7 11 13]))
