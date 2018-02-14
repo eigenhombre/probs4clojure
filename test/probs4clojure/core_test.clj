d08cfceea29e5e0373cb69d58af4f9e16590def8
John Jacobsen <john@mail.npxdesigns.com>
2018-02-13 21:41:45 -0600

    Improve #115.

--- test/probs4clojure/core_test.clj
+++ test/probs4clojure/core_test.clj
@@ -1771,6 +1771,29 @@
          ["this" "is" "a" "sentence" "i" "wrote"])))
 
 
+;; ### Problem 115: <a href="http://www.4clojure.com/problem/115">The Balance of N</a>
+;; The cheap and dirty trick here is to use the string representation
+;; of the number to get the digits; after that, it's a matter of
+;; chopping the sequence in half and adding the digits in each half.
+(problem 115
+  (fn [x]
+    (let [digits (map #(Integer. (str %)) (str x))
+          pivot (quot (count digits) 2)]
+      (= (apply + (take pivot digits))
+         (apply + (take pivot (reverse digits))))))
+  (= true (__ 11))
+  (= true (__ 121))
+  (= false (__ 123))
+  (= true (__ 0))
+  (= false (__ 88099))
+  (= true (__ 89098))
+  (= true (__ 89089))
+  (= (take 20 (filter __ (range)))
+     [0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101])
+  (= (take 20 (filter __ (range)))
+     [0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101]))
+
+
 ;; ### Problem 117: <a href="http://www.4clojure.com/problem/117">For Science!</a>
 ;;
 ;; I treat this as another graph search problem, where connections
--- test/probs4clojure/solved_to_migrate.clj
+++ test/probs4clojure/solved_to_migrate.clj
@@ -567,47 +567,6 @@
 
 
 
-;; ;; The Balance of N
-;; ;;
-;; ;; Difficulty:Medium
-;; ;; Topics:math
-;; ;;
-;; ;; A balanced number is one whose component digits have the same sum
-;; ;; on the left and right halves of the number. Write a function which
-;; ;; accepts an integer n, and returns true iff n is balanced.
-
-;; ;; (= true (__ 11))
-
-;; ;; (= true (__ 121))
-
-;; ;; (= false (__ 123))
-
-;; ;; (= true (__ 0))
-
-;; ;; (= false (__ 88099))
-
-;; ;; (= true (__ 89098))
-
-;; ;; (= true (__ 89089))
-
-;; ;; (= (take 20 (filter __ (range)))
-;; ;;    [0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101])
-
-
-;; (defn balanced [n]
-;;   (let [digits (map #(Integer/parseInt (str %)) (str n))
-;;         len (count digits)
-;;         pivot (int (/ len 2))
-;;         [l r] (split-at pivot digits)
-;;         r (if (even? len) r (rest r))]
-;;     (= (apply + l) (apply + r))))
-
-;; (expect true (balanced 11))
-
-;; (expect (take 20 (filter balanced (range)))
-;;         [0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101])
-
-
 ;; ;; Difficulty:Easy
 ;; ;; Topics:set-theory
 ;; ;; Given a set of sets, create a function which returns true if no two
